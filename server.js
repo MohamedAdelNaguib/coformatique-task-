0c8a7e8d2b99491b7ef417a908089a7cfb7c13f3
Mohamed Adel Naguib <47299025+MohamedAdelNaguib@users.noreply.github.com>
2020-09-10 20:43:01 +0200

    Setting up the App and the server side

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+
+const app = express()
+
+// Init middleware
+app.use(express.json())
+
+// added cors access
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('homepage')
+})
+
+
+// 500 internal server error handler
+app.use((err, _req, res, next) => {
+  if (err.statusCode === 400) {
+    return next(err)
+  }
+
+  return res.status(500).json({
+    data: null,
+    err: process.env === 'production'
+      ? null
+      : err,
+    msg: process.env === 'production'
+      ? 'Error!'
+      : '500 Internal Server Error'
+  })
+})
+
+// 400 error handler
+app.use((err, _req, res, next) => {
+  if (err.statusCode === 404) {
+    return next()
+  }
+
+  return res.status(400).json({
+    data: null,
+    err: process.env === 'production'
+      ? null
+      : err,
+    msg: '400 Bad Request'
+  })
+})
+
+// 404 error handler
+app.use((_req, res) => res.status(404)
+  .json({
+    data: null,
+    status: 'Error',
+    msg: 'Error 404: We can not find what you are looking for'
+  }))
+
+
+let port = 8000
+app.listen(port, () => { console.log(`Server is up and running on port ${port}`) })
